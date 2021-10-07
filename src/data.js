bf0c2cdf62fd9c59b593791dbb4073f3b35c6a81
humneankush <humneankush@gmail.com>
2021-10-07 16:10:46 +0530

    ant design project link added

--- src/data.js
+++ src/data.js
@@ -1,39 +1,34 @@
 export const projects = [
-    {
-      title: "Blog App",
-      subtitle: "MERN Stack",
-      description:
-        "A Blog website to post your blog with backend",
-      image: "assets/blog.png",
-      link: "https://mernblogapps.herokuapp.com/",
-    },
-    {
-      title: "React Chat",
-      subtitle: "Unichat",
-      description:
-        "A simple chat app that works on react-chat-engine ",
-      image: "assets/chat.png",
-      link: "https://chatmessage.netlify.app",
-    },
-    {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
-    },
-    {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-4.gif",
-      link: "https://pythonbootcamp.com",
-    },
-  ];
+  {
+    title: "Blog App",
+    subtitle: "MERN Stack",
+    description: "A Blog website to post your blog with backend",
+    image: "assets/blog.png",
+    link: "https://mernblogapps.herokuapp.com/",
+  },
+  {
+    title: "React Chat",
+    subtitle: "Unichat",
+    description: "A simple chat app that works on react-chat-engine ",
+    image: "assets/chat.png",
+    link: "https://chatmessage.netlify.app",
+  },
+  {
+    title: "CryptoWorld",
+    subtitle: "React and Redux tool kit",
+    description:
+      "Application using which fetching crypto data from the api for the design i used ant design",
+    image: "assests/crypto.png",
+    link: "https://jovial-meninsky-cfc6a2.netlify.app/exchanges",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
 
-
-  export const skills = [
-      "MERN" ,"PYTHON" ,"C","REST"
-  ]
+export const skills = ["MERN", "PYTHON", "C", "REST"];
